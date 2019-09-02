fd17ec0ccf42b63186f9a3282ada4daa7095d42d
zahra memar <zahra.memar72@gmail.com>
2019-09-02 16:01:43 +0200

    imporve poll file and use react func instead of class

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Poll extends Component {
-  render() {
-    return (
-      <div className="flex mt2 items-start">
-        <div className="ml1">
-          {this.props.poll ? <div>{this.props.poll.description}</div> : ""}
+export default ({ poll }) => {
+  return (
+    <div className="flex mt2 items-start">
+      <div className="ml1">
+        {poll ? <div>{poll.description}</div> : ""}
 
-          <div className="f6 lh-copy gray">
-            votes:
-            {this.props.poll && this.props.poll.votes.length > 0 ? (
-              this.props.poll.votes.map(vote => (
-                <span key={vote.id} className="ma1">
-                  {vote.dificulty}
-                </span>
-              ))
-            ) : (
-              <span>not voted yet</span>
-            )}
-          </div>
+        <div className="f6 lh-copy gray">
+          votes:
+          {poll && poll.votes.length > 0 ? (
+            poll.votes.map(vote => (
+              <span key={vote.id} className="ma1">
+                {vote.dificulty}
+              </span>
+            ))
+          ) : (
+            <span>not voted yet</span>
+          )}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
