e429a9387e5af2824c2eab2c3598d2a81881a21d
zahra memar <zahra.memar72@gmail.com>
2019-09-03 10:46:49 +0200

    add some style to create poll

--- src/components/CreatePoll.js
+++ src/components/CreatePoll.js
@@ -16,24 +16,38 @@ export default ({ history }) => {
 
   return (
     <div>
-      <div className="flex flex-column mt3">
-        <input
-          className="mb2"
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-          type="text"
-          placeholder="A description for the poll"
-        />
+      <div className="mw8 center br2 ba b--light-blue bg-lightest-blue">
+        <div className="dt-ns dt--fixed-ns w-100">
+          <div className="pa3 pa4-ns dtc-ns v-mid">
+            <div>
+              <input
+                className="mb2 f6 tc db w-100 pv3"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                type="text"
+                placeholder="A description for the poll"
+              />
+            </div>
+          </div>
+          <div className="pa3 pa4-ns dtc-ns v-mid">
+            <Mutation
+              mutation={CREATE_POLL_MUTATION}
+              variables={{ description }}
+              onCompleted={() => history.push("/")}
+            >
+              {createPollMutation => (
+                <button
+                  className="no-underline f6 tc db w-100 pv3 bg-animate bg-blue hover-bg-dark-blue white br2"
+                  onClick={createPollMutation}
+                >
+                  Submit
+                </button>
+              )}
+            </Mutation>
+          </div>
+        </div>
       </div>
-      <Mutation
-        mutation={CREATE_POLL_MUTATION}
-        variables={{ description }}
-        onCompleted={() => history.push("/")}
-      >
-        {createPollMutation => (
-          <button onClick={createPollMutation}>Submit</button>
-        )}
-      </Mutation>
+      <div className="flex flex-column mt3"></div>
     </div>
   );
 };
